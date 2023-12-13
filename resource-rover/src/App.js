0fb01ae658a2a2217af964797f04bac13e85de67
Mohamed Tawfik <mohamedatef9937@gmail.com>
2023-12-13 12:10:55 +0100

    adding components pages and api services

--- resource-rover/src/App.js
+++ resource-rover/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RegistrationForm from './components/RegistrationForm';
+import LoginForm from './components/LoginForm';
+import UserProfilePage from './components/UserProfilePage';
+import ChooseTopicPage from './pages/ChooseTopicPage';
+import NewTopicForm from './components/NewTopicForm';
+import ChooseResourcePage from './pages/ChooseResourcePage';
+import NewResourceForm from './components/NewResourceForm';
+import ResourcePage from './pages/ResourcePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/register" component={RegistrationForm} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/profile" component={UserProfilePage} />
+        <Route path="/topics" exact component={ChooseTopicPage} />
+        <Route path="/topics/new" component={NewTopicForm} />
+        <Route path="/resources" exact component={ChooseResourcePage} />
+        <Route path="/resources/new" component={NewResourceForm} />
+        <Route path="/resource/:id" component={ResourcePage} />
+    </Router>
   );
 }
 
 export default App;
+
---
+++ resource-rover/src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegistrationFrom = () => {
+  return (
+    <div>
+      <h1>Welcome to Registration of Resource Rover</h1>
+    </div>
+  );
+};
+
+export default RegistrationFrom;
